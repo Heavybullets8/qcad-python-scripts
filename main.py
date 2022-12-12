525aa2ee2ba6ead0f1531ae11cd942d8ced10b7b
Heavybullets8 <heavybullets8@gmail.com>
2022-12-12 07:10:37 -0700

    remove test file

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import os
 from general_tubes import *
 from sliders import *
-from test import *
 
 
 print("1) General Tubes")
 print("2) Sliders")
-print("3) Testing")
 print()
 
 tube_type = ""
-while tube_type not in [1,2,3]:
+while tube_type not in [1,2]:
     tube_type = int(input("What type of blueprint are you making?(1 or 2): "))
     if tube_type == 1:
         general_tubes()
     elif tube_type == 2:
         sliders()
-    elif tube_type == 3:
-        testing()
     else: 
         print("Error: {tube_type} was not an option, try again..".format(tube_type=tube_type))
 
